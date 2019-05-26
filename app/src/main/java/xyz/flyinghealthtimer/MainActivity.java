6312f7565871ed394adcd908074ed3eed8c33863
Yunjeong Goo <heidiyun.goo@gmail.com>
2019-05-26 21:47:08 +0900

    Floating Window만 띄움. 기능 구현 X

--- app/src/main/java/xyz/flyinghealthtimer/MainActivity.java
+++ app/src/main/java/xyz/flyinghealthtimer/MainActivity.java
@@ -1,10 +1,16 @@
 package xyz.flyinghealthtimer;
 
+import android.annotation.TargetApi;
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -12,12 +18,14 @@ import trikita.log.Log;
 
 
 public class MainActivity extends AppCompatActivity {
+    private static final int OVERLAY_PERMISSION_REQUEST_CODE = 100;
+    public final static int REQUEST_CODE = 3333;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
+        getPermissionAndStartService(true);
 
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -29,8 +37,8 @@ public class MainActivity extends AppCompatActivity {
         FragmentController.newFragment(new MainFragment(), R.layout.fragment_main, false);
         onNewIntent(getIntent());
 
-
     }
+
 
 
     @Override
@@ -38,20 +46,20 @@ public class MainActivity extends AppCompatActivity {
         super.onNewIntent(intent);
         // getIntent() should always return the most recent
         setIntent(intent);
-        if(getIntent() != null && getIntent().getExtras() != null){
+        if (getIntent() != null && getIntent().getExtras() != null) {
             int id = getIntent().getIntExtra("timer_id", -1);
             boolean stop = getIntent().getBooleanExtra("stop", false);
             Log.d("onNewIntent id", id, "stop", stop);
 
-            if(stop){
+            if (stop) {
                 stopService(new Intent(this, TimerService.class));
                 FragmentController.backFragmet();
                 return;
             }
             List<TimerModel> timerModelList = TimerApi.getListTimers(this);
-            for( TimerModel tm : timerModelList){
-                if(tm.id == id){
-                    if(getSupportFragmentManager().findFragmentByTag(R.layout.fragment_timer+"") == null) {
+            for (TimerModel tm : timerModelList) {
+                if (tm.id == id) {
+                    if (getSupportFragmentManager().findFragmentByTag(R.layout.fragment_timer + "") == null) {
                         FragmentController.newFragment(new TimerFragment(tm), R.layout.fragment_timer, true);
                     }
                 }
@@ -71,9 +79,33 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    @TargetApi(Build.VERSION_CODES.M)
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == OVERLAY_PERMISSION_REQUEST_CODE) {
+            getPermissionAndStartService(false);
+        }
+    }
+    private void getPermissionAndStartService(boolean isShowOverlayPermission) {
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.LOLLIPOP_MR1) {
+//            startService();
+        } else {
+            if (Settings.canDrawOverlays(this)) {
+//                startService();
+                return;
+            }
+            if (isShowOverlayPermission) {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + this.getPackageName()));
+                startActivityForResult(intent, OVERLAY_PERMISSION_REQUEST_CODE);
+            }
+        }
+    }
 
 
+    private void startService() {
 
+//        finishAndRemoveTask();
+    }
 
 
 }
